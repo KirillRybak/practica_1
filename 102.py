2c5cf18b8a14f1cc9332bd3949b4269933258460
KirillRybak <kiryz.rybak.00@mail.ru>
2023-01-09 14:02:03 +0300

    homework_12

---
+++ 102.py
@@ -0,0 +1,3 @@
+x = int(input("Введите трёхзначное число  "))
+print(int(str(x)[0]) + int(str(x)[1]) + int(str(x)[2]))
+print(int(str(x)[0]) * int(str(x)[1]) * int(str(x)[2]))
---
+++ 103.py
@@ -0,0 +1,1 @@
+print(*range(int(input("Нижний порог ")), int(input("Верхний порог ")) + 1, int(input("Шаг "))))
