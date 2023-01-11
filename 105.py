1ea5069298debbcdcd898a844ebb26966fbc9db9
KirillRybak <kiryz.rybak.00@mail.ru>
2023-01-11 12:43:23 +0300

    homework_12

--- 105.py
+++ 105.py
@@ -1,9 +1,9 @@
 number1 = int(input("Введите число: "))
 number2 = 0
-while n1 > 0:
+while number1 > 0:
     x = number1%10
-    number1 = n1//10
-    n2 = number2 * 10 + x
+    number1 = number1//10
+    number2 = number2 * 10 + x
 print(number2)
 
 
