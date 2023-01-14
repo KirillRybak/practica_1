0d5fc4e6f9cb9884b77123c2a09932720d61588d
KirillRybak <kiryz.rybak.00@mail.ru>
2023-01-14 11:15:04 +0300

    homework_12

---
+++ 111.py
@@ -0,0 +1,9 @@
+try:
+    a = [int(i) for i in input("Заполните список (через пробел): ").split()]
+    s = 0
+    for i in range(len(a)):
+        if a[i] % 2 == 0 and a[i] > 0:
+            s += a[i]
+    print(s)
+except:
+    print("Упс, что то пошло не так")
