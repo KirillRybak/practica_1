6480d3c9b1de15b0fcfc05bf6d49abd635111c99
KirillRybak <kiryz.rybak.00@mail.ru>
2023-01-15 10:35:13 +0300

    homework_12

---
+++ 112.py
@@ -0,0 +1,15 @@
+
+try:
+    import statistics
+    a = [int(i) for i in input("Заполните список (через пробел): ").split()]
+    n = statistics.mean(a)
+    for z in range(a[0], len(a), 1):
+        if z<n:
+            print(z, end=" | ")
+except:
+    print("Упс, что то пошло не так")
+
+
+
+
+
