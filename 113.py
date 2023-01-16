c989a2333df60af32af4ef1964701acdf91692ce
KirillRybak <kiryz.rybak.00@mail.ru>
2023-01-16 11:43:08 +0300

    homework_12

--- 113.py
+++ 113.py
@@ -1,6 +1,10 @@
-a = [int(i) for i in input("Заполните список (через пробел): ").split()]
-x = min(a)
-print(x, end=" | ")
-a.remove(x)
-c = min(a)
-print(c)
+try:
+    a = [int(i) for i in input("Заполните список (через пробел): ").split()]
+    x = min(a)
+    print(x, end=" | ")
+    a.remove(x)
+    c = min(a)
+    print(c)
+    a.remove(c)
+except:
+    print("Упс, что то пошло не так")
