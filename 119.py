e612ae53bef4d348f5f7cf3364df7a700a067d54
KirillRybak <kiryz.rybak.00@mail.ru>
2023-01-23 12:03:53 +0300

    homework_12

--- 119.py
+++ 119.py
@@ -1,9 +1,26 @@
-n = int(input("Введите кол-во множеств: "))
-i = 3
-x = []
-while i<n:
-    i += 1
-    m = {"пусто"}
-    if i%3==0 and m:
-        m = str(i)+ " " + str({"пусто"})
-        print(m, end= " | ")
+from random import randint
+
+n = int(input("n="))
+
+sets = [None]
+
+for i in range(1, n + 1):
+    t = []
+    for _ in range(10):
+        t.append(randint(-10, 10))
+    sets.append(set(t))
+
+print("Исходные множества:")
+for i in range(1, n + 1):
+    print(i, ":", *sets[i])
+
+res = sets[3]
+i = 6
+while i <= n:
+    res = res & sets[i]
+    i += 3
+res = res - sets[1]
+print("Результат:")
+print(*res)
+
+
