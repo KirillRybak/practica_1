75c9feedc433966c143464234f6c1f55feef449a
KirillRybak <kiryz.rybak.00@mail.ru>
2023-01-31 11:42:28 +0300

    homework_12

--- 125.py
+++ 125.py
@@ -1,12 +1,10 @@
 from random import randint
 
+n = int(input("Введите минимальное значение: "))
+m = int(input("Введите максимальное значение: "))
 def hello(n,m):
-    n = int(input("Введите минимальное значение: "))
-    m = int(input("Введите максимальное значение: "))
+
     b = int(input("Введите кол-во значений в массиве: "))
     x =[randint(n,m) for i in range(b)]
     print(x)
-hello(-10,10)
-
-
-
+hello(n,m)
