4ff10bd35105e42c093176c8a92198b20f73434b
KirillRybak <kiryz.rybak.00@mail.ru>
2023-02-05 16:53:18 +0300

    homework

--- 130.py
+++ 130.py
@@ -1,9 +1,14 @@
-
+def fan(func):
+    def winner():
+        print("Вообще без понятия, как это делать")
+        func()
+    return winner
+@fan
 def hello():
     name = input("Введите имя: ")
     x = "Hello, " + name
     x1 = x.upper()
     return print(x,x1)
 
-hello()
+fan(hello)
 
