d562e725ee34e3611d2db1549acb88d2fbf06efc
KirillRybak <kiryz.rybak.00@mail.ru>
2023-02-07 13:47:44 +0300

    homework

--- 131.py
+++ 131.py
@@ -3,6 +3,7 @@ class Objects:
     def __init__(self,Obj1,Obj2):
         self.Obj1 = str(Obj1)
         self.Obj2 = str(Obj2)
+        self.age = 22
 
     def object(self):
         print("Имя: " + self.Obj1 + "\n" + "Возраст: " + self.Obj2 + "\n")
@@ -12,10 +13,11 @@ Proj2 = Objects("Василий",43)
 Proj3 = Objects("Никита",18)
 Proj4 = Objects("Ника", 20)
 Proj5 = Objects("Яна", 27)
-Proj5.object()
-Proj4.object()
+print(Proj5.Obj1)
+print(Proj4.Obj2)
 Proj3.object()
 Proj2.object()
 Proj1.object()
+print(Proj1.age)
 
 
