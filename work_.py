11e21381d300852fdd8e530106da4d59e3939c8e
KirillRybak <kiryz.rybak.00@mail.ru>
2023-02-11 13:30:02 +0300

    homework

--- work_.py
+++ work_.py
@@ -2,16 +2,11 @@ class animal:
     def __init__(self,animal):
         self.animal = animal
 
-    def _leo(self):
+    def leo(self):
         print("Привет, я " + self.animal)
-    def _spe(self,speed):
+    def spe(self,speed):
         print("Могу разогнаться до " + str(speed) + " км/ч")
-animal1 = animal("Леопард")
-animal1._leo()
-
-obj1 = animal("loo")
-
-
-
+    def Weight(self,x):
+        print("Мой вес " + str(x) + " кг")
 
 
