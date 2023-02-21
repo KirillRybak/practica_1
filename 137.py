76ed86b9bedace2674b452e4de4ea2d7978c1dbe
KirillRybak <kiryz.rybak.00@mail.ru>
2023-02-21 13:06:12 +0300

    homework

---
+++ 137.py
@@ -0,0 +1,17 @@
+import sqlite3 as sq
+conn = sq.connect('conf.db')
+curs = conn.cursor()
+
+zap1 = """SELECT * FROM UCHA;"""
+
+curs.execute(zap1)
+vyb = curs.fetchall()
+print(vyb)
+
+zap2 = """SELECT * FROM PERS WHERE Город='Минск';"""
+curs.execute(zap2)
+vyb2 = curs.fetchall()
+print(vyb2)
+
+curs.close()
+conn.close()
